858173e9965687c64353828f0654981a7986507e
MatthewKK01 <mateyavelashvili@gmail.com>
2025-07-04 19:21:00 +0400

    updated server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  auth:{
+     secret: env('JWT_SECRET'), 
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
